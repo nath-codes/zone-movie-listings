193435d8a7139470c436761f1fe045dd5131eff1
Nathan Tredgett <nathantredgett@gmail.com>
2018-09-04 21:23:47 +0100

    removed class attribute from zone logo as now longer needed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,12 +15,10 @@ const Header = () => (
         height="32"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 31 32"
-        class="jsx-2735423973"
       >
         <path
           fill="#fff"
           d="M6.83772.75172L2.0797 7.8679h14.00937L0 31.9072h25.16186l4.79772-7.18017H14.9798L31 .75172z"
-          class="jsx-2735423973"
         />
       </svg>
     </Container>
